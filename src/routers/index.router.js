a3d9142f743635be1976349e91f6894e5d8e56e4
JhonCamargo07 <jhonalexcamargo07@gmail.com>
2023-07-04 17:29:15 -0500

    Se realizan ajustes al template

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,15 +1,8 @@
 const Router = require('express');
+const { login } = require('../controllers/index.controller');
 const router = Router();
 
-router.post('/', (req, res) => {
-	const { user, pass } = req.body;
-
-	if (!user || !pass) {
-		return res.status(200).json({ success: false, message: 'Data not found' });
-	}
-
-	return res.status(200).json({ success: true, message: 'Logged in session' });
-});
+router.post('/', login);
 
 router.use((req, res) => {
 	res.status(404).json({ success: false, message: 'Page no found' });
